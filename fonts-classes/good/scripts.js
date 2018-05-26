9fef89ab6de9eb71b7891b1231f02328be1c38a5
Jesús Olazagoitia <jesus.designer@gmail.com>
2018-05-26 21:15:07 +0200

    add speed index

--- fonts-classes/good/scripts.js
+++ fonts-classes/good/scripts.js
@@ -12,13 +12,8 @@ f&&-1!=g||-1!=f&&-1!=h||-1!=g&&-1!=h)(b=f!=g&&f!=h&&g!=h)||(null===B&&(b=/AppleW
 g=n.a.offsetWidth,h=p.a.offsetWidth,u();q=setTimeout(I,50)}}var e=new r(k),n=new r(k),p=new r(k),f=-1,g=-1,h=-1,v=-1,w=-1,x=-1,d=document.createElement("div");d.dir="ltr";t(e,L(c,"sans-serif"));t(n,L(c,"serif"));t(p,L(c,"monospace"));d.appendChild(e.a);d.appendChild(n.a);d.appendChild(p.a);document.body.appendChild(d);v=e.a.offsetWidth;w=n.a.offsetWidth;x=p.a.offsetWidth;I();z(e,function(a){f=a;u()});t(e,L(c,'"'+c.family+'",sans-serif'));z(n,function(a){g=a;u()});t(n,L(c,'"'+c.family+'",serif'));
 z(p,function(a){h=a;u()});t(p,L(c,'"'+c.family+'",monospace'))})})};"object"===typeof module?module.exports=A:(window.FontFaceObserver=A,window.FontFaceObserver.prototype.load=A.prototype.load);}());
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('Dom ready!');
-});
-
 function onFontLoaded() {
-    console.log('Font loaded!');
+    console.log('Fonts loaded!');
     const dom = document.documentElement;
     dom.classList.remove('initial-font');
     dom.classList.add('font-loaded');
